2af0d7583ecc669ddf89cbe31d8ec21c87ad0fd9
hannahkoske <hannah.koske@andela.com>
2015-12-15 20:33:52 +0300

    [#110135358] Change initial test paths to conform to new test folder structure

--- document-manager.spec.js
+++ document-manager.spec.js
@@ -1,4 +1,4 @@
-require('./spec/src/roles.spec.js');
-require('./spec/src/types.spec.js');
-require('./spec/src/users.spec.js');
-require('./spec/src/documents.spec.js');
+require('./spec/server/src/roles.spec.js');
+require('./spec/server/src/types.spec.js');
+require('./spec/server/src/users.spec.js');
+require('./spec/server/src/documents.spec.js');
