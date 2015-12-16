a546a92fe35b041945a0506caf2bf48f59898c79
hannahkoske <hannah.koske@andela.com>
2015-12-16 14:53:08 +0300

    [#110238474] Set up angular app

---
+++ app/scripts/application.js
@@ -0,0 +1,54 @@
+(function() {
+  'use strict';
+  angular.module('docman.controllers', []);
+  angular.module('docman.services', []);
+  angular.module('docman.filters', []);
+  angular.module('docman.directives', []);
+
+  window.app = angular.module('docman', [
+    'docman.controllers',
+    'docman.services',
+    'docman.filters',
+    'docman.directives',
+    'ngRoute',
+    'ui.router',
+    'ngResource',
+    'ngMaterial',
+    'angularFileUpload'
+  ]);
+
+  window.app.run([
+    function() {
+
+      }
+  ]);
+
+  window.app.config(['$stateProvider', '$httpProvider',
+   '$urlRouterProvider', '$locationProvider', '$mdThemingProvider',
+    function($stateProvider, $httpProvider, $urlRouterProvider,
+     $locationProvider, $mdThemingProvider) {
+      // For any unmatched url, redirect to /state1
+      $urlRouterProvider.otherwise('/404');
+
+      // Now set up the states
+      $mdThemingProvider.theme('default')
+        .primaryPalette('blue')
+        .accentPalette('deep-orange')
+        .backgroundPalette('grey', {
+          default: '200'
+        });
+
+      $stateProvider
+        .state('home', {
+          url: '/',
+          templateUrl: 'views/home.html'
+        })
+        .state('404', {
+          url: '/404',
+          templateUrl: 'views/404.html'
+        });
+      $locationProvider.html5Mode(true);
+    }
+  ]);
+
+})();
