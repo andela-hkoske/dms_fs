de8fa0beca32473c0f9218c45f51c70204f7faa6
hannahkoske <hannah.koske@andela.com>
2015-12-21 09:57:26 +0300

    [#110317526] Add utils service

---
+++ app/scripts/services/utils.js
@@ -0,0 +1,24 @@
+angular.module('docman.services', [])
+  .service('Utils', function($mdToast, $mdDialog) {
+    this.toast = function(msg) {
+      $mdToast.show($mdToast.simple().content(msg));
+    };
+    this.dialog = function(title, message, event, callback) {
+      $mdDialog.show(
+          $mdDialog.confirm()
+          .parent(angular.element(document.body))
+          .clickOutsideToClose(true)
+          .title(title)
+          .content(message)
+          .ariaLabel('Utils Dialog Service')
+          .ok('OK')
+          .cancel('CANCEL')
+          .targetEvent(event)
+        )
+        .then(function() {
+          if (typeof callback === 'function') {
+            callback();
+          }
+        }, function() {});
+    };
+  });
