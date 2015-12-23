41a05e98321a622118bb011184e7d7e9e5c38664
hannahkoske <hannah.koske@andela.com>
2015-12-23 15:09:19 +0300

    [#110317516] Add timeout injection and error callback in save user method

--- app/scripts/controllers/signup.js
+++ app/scripts/controllers/signup.js
@@ -1,23 +1,24 @@
 angular.module('docman.controllers')
-  .controller('SignupCtrl',
-    ['$rootScope', '$scope', '$state', 'Users', 'Auth', 'Roles',
-    function($rootScope, $scope, $state, Users, Auth, Roles) {
+  .controller('SignupCtrl', ['$rootScope', '$scope', '$state', '$timeout',
+    'Users', 'Auth', 'Roles',
+    function($rootScope, $scope, $state, $timeout, Users, Auth, Roles) {
       $scope.user = {};
       Roles.query(function(res) {
         $scope.roles = res;
-        console.log(res);
       });
       $scope.save = function() {
-        Users.save($scope.user, function(err, res) {
-          if (err && !res) {
-            $scope.message = err.message ||
-              'There was a problem signing you up.';
-          } else {
-            Auth.setToken(res.token);
-            $rootScope.currentUser = res;
-            $state.go('login');
-          }
-        });
+        Users.save($scope.user, function() {
+            $scope.message = 'Successfully signed up. You will now' +
+              ' be redirected to login. Login to proceed.';
+            $timeout(function() {
+              $state.go('login');
+            }, 3000);
+          },
+          function(err) {
+            $scope.message = err.data.message ||
+              'There was a problem signing you up. ' +
+              'Try again with different credentials.';
+          });
       };
     }
   ]);
